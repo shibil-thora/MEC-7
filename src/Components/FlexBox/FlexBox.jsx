8a6e5be6683a01d388224e1733975da2f654436d
shibil-thora <shibilallakkat123@gmail.com>
2024-11-17 11:49:34 +0530

    added design for main page

--- src/Components/FlexBox/FlexBox.jsx
+++ src/Components/FlexBox/FlexBox.jsx
@@ -5,14 +5,14 @@ const FlexBox = ({ heading, photo, video, gentsCount, ladiesCount }) => {
     <div className="flex flex-col md:flex-row p-4 border-b">
       <div className="flex flex-col md:flex-row w-full">
         <div className="md:w-1/2 p-2">
-          <h2 className="text-xl font-semibold mb-2">{heading}</h2>
+          <h2 className="text-4xl font-semibold mb-2">{heading}</h2>
           <img
             src={photo}
             alt="photo"
             className="w-full h-96 object-cover mb-2"
           />
           <div className="text-gray-600 text-xl mt-5">
-            Gents: <span className="w-25 bg-red-500 p-3 text-3xl font-bold text-white">{gentsCount}</span> Lead by: <span className="font-semibold">Mohammad Ali</span> Ladies: {ladiesCount} Lead by: 
+            Gents: <span className="w-25 bg-red-500 p-3 text-3xl font-bold text-white">{gentsCount}</span> Lead by: <span className="font-semibold">Mohammad Ali</span> Ladies: <span className="w-25 bg-red-500 p-3 text-3xl font-bold text-white">{ladiesCount}</span> Lead by: 
           </div>
         </div>
         <div className="md:w-1/2 p-2">
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], 
+  server: {
+    host: '192.168.1.8',
+    port: 5173
+  }
 })
