1b04df7d1137518ec9dfa36a38a8c514e2a81a5e
shibil-thora <shibilallakkat123@gmail.com>
2024-11-24 20:50:08 +0530

    created funcitons for storing session

--- fastapi_backend/test.py
+++ fastapi_backend/test.py
@@ -1,12 +1,16 @@
-import jwt
+import jwt 
+from dotenv import load_dotenv 
+import os
+
+load_dotenv()
 
 # Secret key for encoding and decoding JWT
-SECRET_KEY = "your_secret_key"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Function to create a JWT token without expiration
-def create_token(username):
+def create_token(mobile_number):
     payload = {
-        "username": username
+        "mobile_number": mobile_number
     }
     token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
     return token
@@ -15,7 +19,7 @@ def create_token(username):
 def decode_token(token):
     try:
         decoded_payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
-        username = decoded_payload.get("username")
+        username = decoded_payload.get("mobile_number")
         return username
     except jwt.InvalidTokenError:
         print("Invalid token")
@@ -23,7 +27,7 @@ def decode_token(token):
 
 # Example usage
 if __name__ == '__main__':
-    username = "example_user"
+    username = "6238748856"
     token = create_token(username)
     print("Generated Token:", token)
     
